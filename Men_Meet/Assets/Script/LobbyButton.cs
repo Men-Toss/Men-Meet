7ccd544edf487b4ce5d51a6094f7bf77ef8cd318
Kim SungJin <asdf74591@gmail.com>
2022-04-26 22:30:57 +0900

    Update

--- Men_Meet/Assets/Script/LobbyButton.cs
+++ Men_Meet/Assets/Script/LobbyButton.cs
@@ -10,11 +10,13 @@ public class LobbyButton : MonoBehaviour
     [SerializeField] private List<RectTransform> ConfirmedUIobjects=new List<RectTransform>();
     [SerializeField] private Sprite[] CharLogomenArr;
     [SerializeField] private Sprite[] CharLogoWomenArr;
+    public float Space = 10f;
     public CharChoosePrefab  choosePrefab;
     private int[] CharChooseRectX = new int[3] { -300, 0, 300 };
     public void CharLoadMen()
     {
         ConfirmedUIobjects.Clear();
+        float y = 0f;
         for (int i = 0; i < CharLogomenArr.Length; i++)
         {
             choosePrefab.SetImg(CharLogomenArr[i]);
@@ -22,12 +24,29 @@ public class LobbyButton : MonoBehaviour
             ConfirmedUIobjects.Add(newButton);
         }
 
-        for (int i = 0; i < ConfirmedUIobjects.Count; i++)
-            ConfirmedUIobjects[i].anchoredPosition = new Vector2(CharChooseRectX[i%3], -40 + (-240*(i/3)));
+        for (int i = 0; i < ConfirmedUIobjects.Count; i++)  
+            ConfirmedUIobjects[i].anchoredPosition = new Vector2(CharChooseRectX[i % 3], -40 + (-240 * (i / 3)));
 
-            //scrollRect.content.sizeDelta = new Vector2(scrollRect.content.sizeDelta.x, -40 + (-240 * ((ConfirmedUIobjects.Count - 1) / 3)));
-
+        y= -40 + (-240 * ((ConfirmedUIobjects.Count-1) / 3));
+        scrollRect.content.sizeDelta = new Vector2(scrollRect.content.sizeDelta.x, y);
     }
+
+    public void CharLoadWomen() 
+    {
+        ConfirmedUIobjects.Clear();
+        for (int i = 0; i < CharLogoWomenArr.Length; i++)
+        {
+            choosePrefab.SetImg(CharLogoWomenArr[i]);
+            var newButton = Instantiate(choosePrefab, scrollRect.content).GetComponent<RectTransform>();
+            ConfirmedUIobjects.Add(newButton);
+        }
+
+        for (int i = 0; i < ConfirmedUIobjects.Count; i++)
+            ConfirmedUIobjects[i].anchoredPosition = new Vector2(CharChooseRectX[i % 3], -40 + (-240 * (i / 3)));
+
+        
+    }
+
     public void LogOutCLick() => SceneManager.LoadScene(0);
     public void StartClick() => SceneManager.LoadScene(3);
 }
