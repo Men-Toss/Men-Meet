cfcd692b742f26fdde4c901169ac4c2ccce748a5
Kim SungJin <asdf74591@gmail.com>
2022-05-10 16:26:26 +0900

    카메라 움직임 / RPC 적용

---
+++ Men_Meet/Assets/Script/LobbySceneScript/LobbySoundScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbySoundScript : MonoBehaviour
+{
+    public void setSound(int index)
+    {
+        
+    }
+}
