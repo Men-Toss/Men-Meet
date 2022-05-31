d6988a21f830bbae418449f4d0952a6f26096507
Kim SungJin <asdf74591@gmail.com>
2022-05-31 15:00:47 +0900

    최적화

--- Men_Meet/Assets/Script/ListScript/IntroducePrefabData.cs
+++ Men_Meet/Assets/Script/ListScript/IntroducePrefabData.cs
@@ -11,4 +11,20 @@ public class IntroducePrefabData : MonoBehaviour
     public int Code1;
     public int Code2;
     public void setColor(Color c)=>gameObject.GetComponent<Image>().color = c;
+
+    public void ClickThisIntroducePrefab()
+    {
+        if (this.Code1 == 1 && this.Code2 == 0)
+            GameObject.Find("ButtonManager").GetComponent<LoadingButtonManage>().ClickIntroduceImage(0);
+        if (this.Code1 == 1 && this.Code2 == 1)
+            GameObject.Find("ButtonManager").GetComponent<LoadingButtonManage>().ClickIntroduceImage(1);
+        if (this.Code1 == 2 && this.Code2 == 0)
+            GameObject.Find("ButtonManager").GetComponent<LoadingButtonManage>().ClickIntroduceImage(2);
+        if (this.Code1 == 2 && this.Code2 == 1)
+            GameObject.Find("ButtonManager").GetComponent<LoadingButtonManage>().ClickIntroduceImage(3);
+        if (this.Code1 == 3 && this.Code2 == 0)
+            GameObject.Find("ButtonManager").GetComponent<LoadingButtonManage>().ClickIntroduceImage(4);
+        if (this.Code1 == 4 && this.Code2 == 0)
+            GameObject.Find("ButtonManager").GetComponent<LoadingButtonManage>().ClickIntroduceImage(5);
+    }
 }
